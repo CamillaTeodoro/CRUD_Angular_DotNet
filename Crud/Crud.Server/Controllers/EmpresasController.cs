327711f0076ffe95da3492e699018dbf02d288c9
Camilla Teodoro <camillateodoro@hotmail.com>
2024-11-17 15:54:15 -0300

    create controller for Empresas

---
+++ Crud/Crud.Server/Controllers/EmpresasController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Crud.Server.Models;
+
+namespace Crud.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresasController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public EmpresasController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Empresas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Empresas>>> GetEmpresas()
+        {
+            return await _context.Empresas.ToListAsync();
+        }
+
+        // GET: api/Empresas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Empresas>> GetEmpresas(int id)
+        {
+            if(_context.Empresas == null)
+            {
+                return NotFound();
+            }
+            var empresas = await _context.Empresas.FindAsync(id);
+
+            if (empresas == null)
+            {
+                return NotFound();
+            }
+
+            return empresas;
+        }
+
+        // POST: api/Empresas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Empresas>> PostEmpresas(Empresas empresas)
+        {
+            _context.Empresas.Add(empresas);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmpresas", new { id = empresas.EmpresaId }, empresas);
+        }
+
+        // DELETE: api/Empresas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmpresas(int id)
+        {
+            var empresas = await _context.Empresas.FindAsync(id);
+            if (empresas == null)
+            {
+                return NotFound();
+            }
+
+            _context.Empresas.Remove(empresas);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
